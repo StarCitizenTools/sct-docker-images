39f961167f45f67033b1047c99343f0037c977d5
alistair3149 <alistair31494322@gmail.com>
2024-10-26 16:54:44 -0400

    Drop custom SESP definition for Links to
    It is merged into SESP now. No need for local definition

--- config/LocalSettings.php
+++ config/LocalSettings.php
@@ -490,57 +490,16 @@ $wgDefaultUserOptions['smw-prefs-general-options-show-entity-issue-panel'] = fal
 
 # Semantic Extra Special Properties
 $sespgUseFixedTables = true;
-# Required by Module:DependencyList
-$sespgLocalDefinitions['_LINKSTO'] = [
-    'id'    => '_LINKSTO',
-    'type'  => '_wpg',
-    'alias' => 'sesp-property-links-to',
-    'desc' => 'sesp-property-links-to-desc',
-    'label' => 'Links to',
-    'callback'  => static function(\SESP\AppFactory $appFactory, \SMW\DIProperty $property, \SMW\SemanticData $semanticData ) {
-        $page = $semanticData->getSubject()->getTitle();
-
-        // The namespaces where the property will be added
-        $targetNS = [ 10, 828 ];
-
-        if ( $page === null || !in_array( $page->getNamespace(), $targetNS, true ) ) {
-            return;
-        }
-
-        /** @var \Wikimedia\Rdbms\DBConnRef $con */
-        $con = $appFactory->getConnection();
-
-        $where = [];
-        $where[] = sprintf('pl.pl_from = %s', $page->getArticleID() );
-        $where[] = sprintf('pl.pl_title != %s', $con->addQuotes( $page->getDBkey() ) );
-
-        if ( !empty( $targetNS ) ) {
-            $where[] = sprintf( 'pl.pl_namespace IN (%s)', implode(',', $targetNS ) );
-        }
-
-        $res = $con->select(
-            [ 'pl' => 'pagelinks', 'page' ],
-            [ 'sel_title' => 'pl.pl_title', 'sel_ns' => 'pl.pl_namespace' ],
-            $where,
-            __METHOD__,
-            [ 'DISTINCT' ],
-            [ 'page' => [ 'JOIN', 'page_id=pl_from' ] ]
-        );
-
-        foreach( $res as $row ) {
-            $title = Title::newFromText( $row->sel_title, $row->sel_ns );
-            if ( $title !== null && $title->exists() ) {
-                $semanticData->addPropertyObjectValue( $property,\SMW\DIWikiPage::newFromTitle( $title ) );
-            }
-        }
-    }
-];
-
 $sespgEnabledPropertyList = [
 	'_USERREG',
 	'_USEREDITCNT',
 	'_PAGEIMG',
 	'_LINKSTO'
+];
+# Required by Module:DependencyList
+$sespgLinksToEnabledNamespaces = [
+  10,
+  828
 ];
 
 # Scribunto
