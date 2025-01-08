975039d1b4529955758b97a18fd1d3dfed67901e
alistair3149 <alistair31494322@gmail.com>
2025-01-07 23:39:43 -0500

    Gather Lua performance stat

--- config/LocalSettings.php
+++ config/LocalSettings.php
@@ -900,6 +900,7 @@ $wgRelatedArticlesCardLimit = 6;
 $wgScribuntoDefaultEngine = 'luasandbox';
 $wgScribuntoEngineConf['luasandbox']['memoryLimit'] = 50 * 1024 * 1024; // 50 MB
 $wgScribuntoEngineConf['luasandbox']['cpuLimit'] = 10; // Seconds
+$wgScribuntoGatherFunctionStats = true;
 
 /**
  * Extension:SemanticMediaWiki
