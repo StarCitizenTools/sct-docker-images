277fce810836280367876dbc7739368c5854a453
alistair3149 <alistair31494322@gmail.com>
2025-06-07 17:31:11 -0400

    Remove old local definition for _LINKSTO in LocalSettings.php

--- config/LocalSettings.php
+++ config/LocalSettings.php
@@ -958,6 +958,12 @@ $wgDefaultUserOptions['smw-prefs-general-options-show-entity-issue-panel'] = fal
  *
  * @see https://github.com/SemanticMediaWiki/SemanticExtraSpecialProperties
  */
+
+// Attempt to remove our old local definition for _LINKSTO
+if ( isset( $sespgLocalDefinitions['_LINKSTO'] ) ) {
+	unset( $sespgLocalDefinitions['_LINKSTO'] );
+}
+
 $sespgUseFixedTables = true;
 $sespgEnabledPropertyList = [
 	'_USERREG',
