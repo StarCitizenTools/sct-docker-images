5b951ecfab88a19da83f71a35e990419e6b34ab9
alistair3149 <alistair31494322@gmail.com>
2025-09-26 21:27:48 -0400

    Allow 'self' for base-uri in CSP
    Special:Upload requires `self` directive to be functional

--- config/LocalSettings.php
+++ config/LocalSettings.php
@@ -1462,7 +1462,7 @@ $wgHooks['HtmlPageLinkRendererEnd'][] = function( $linkRenderer, $target, $isKno
  * @see https://doc.wikimedia.org/mediawiki-core/REL1_43/php/classMediaWiki_1_1HookContainer_1_1HookRunner.html#a1bef6100adfea8724efc95072a9c9ef2
  */
 $wgHooks['ContentSecurityPolicyDirectives'][] = function( &$directives, &$policyConfig, $mode ) {
-	$directives[] = "base-uri 'none'";
+	$directives[] = "base-uri 'self'"; // Special:Upload requires base-uri 'self'
 	$directives[] = "form-action 'self'";
 	$directives[] = "frame-ancestors 'none'";
 };
